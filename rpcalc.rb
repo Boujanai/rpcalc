959c74c2487327a0a59721386ad1cf36958eead0
Boujanai <johobuin@yahoo.co.jp>
2015-02-21 23:40:44 +0900

    hai

--- rpcalc.rb
+++ rpcalc.rb
@@ -17,20 +17,25 @@ Plugin.create(:rpcalc) do
 		stack = []
 
 		array.each do |n|
-			if n == "+" then
-				stack[1] = stack[1] + stack[0]
-			elsif n == "-" then
-				stack[1] = stack[1] - stack[0]
-			elsif n == "*" then
-				stack[1] = stack[1] * stack[0]
-			elsif n == "/" then
-				stack[1] = stack[1] / stack[0]
-			else
+			if ("0" <= n) && (n <= "9")
 				stack = [0, n.to_f] + stack
+			elsif stack[1] != nil
+				if n == "+" then
+					stack[1] = stack[1] + stack[0]
+				elsif n == "-" then
+					stack[1] = stack[1] - stack[0]
+				elsif n == "*" then
+					stack[1] = stack[1] * stack[0]
+				elsif n == "/" then
+					stack[1] = stack[1] / stack[0]
+				end
+			else
+				stack = [0, 0]
+				break
 			end
 			stack = stack[1..stack.size - 1]
 		end
-		if stack.size == 1
+		if stack.size == 1 then
 			Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text += " = " + stack[0].to_s
 		else
 			Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text += " Error"
